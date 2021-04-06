230de7266f334562bc54524a77117a5fd9876ff1
akashsundar9122000 <ucs18106@rmd.ac.in>
2021-04-06 22:26:56 +0530

    new commit

--- app/src/main/java/com/studentmanagement/ClassTimeTableActivity.java
+++ app/src/main/java/com/studentmanagement/ClassTimeTableActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class ClassTimeTableActivity extends AppCompatActivity {
-    private CardView mon;
+    private CardView mon,tue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +16,20 @@ public class ClassTimeTableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_class_time_table);
 
         mon=findViewById(R.id.mon);
+        tue=findViewById(R.id.tue);
 
         mon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent gpa_intent = new Intent(ClassTimeTableActivity.this,MondayActivity.class);
+                startActivity(gpa_intent);
+            }
+        });
+
+        tue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gpa_intent = new Intent(ClassTimeTableActivity.this,TuesdayActivity.class);
                 startActivity(gpa_intent);
             }
         });
